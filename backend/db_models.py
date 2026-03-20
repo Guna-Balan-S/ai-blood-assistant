b17e6fb9819debcc82f9a1a387f801224be3b529
Guna Balan <gunabalan376@gmail.com>
2026-03-20 15:12:44 +0530

    added jwt full system

--- backend/db_models.py
+++ backend/db_models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from backend.database.database import Base
 
+# 🔴 Donor Table
 class DonorDB(Base):
     __tablename__ = "donors"
 
@@ -8,3 +9,12 @@ class DonorDB(Base):
     name = Column(String)
     blood = Column(String)
     location = Column(String)
+
+
+# 🔐 User Table (NEW)
+class UserDB(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    password = Column(String)
